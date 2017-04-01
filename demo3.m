6d1a9d25094570a85d12901f13274ae55b3cfd1a
Paul Formanek <pjformanek@gmail.com>
2017-04-01 15:45:39 -0700

    Add files via upload

--- demo3.m
+++ demo3.m
@@ -2,15 +2,18 @@ bsize = 16;
 pframes = zeros(872,2048,3);
 framecount = 0;
 vidReadObj = VideoReader('sintel-2048-stereo.mp4');
-vidReadObj.CurrentTime = 60;
-fname = sprintf('sintel_NTS%d.mp4',bsize);
-vidWriteObj = VideoWriter(fname,'MPEG-4');
+vidReadObj.CurrentTime = 163;
+vidWriteObj = VideoWriter(sprintf('sintel_NTS%d.mp4',bsize),'MPEG-4');
 vidWriteObj.FrameRate = vidReadObj.FrameRate;
 open(vidWriteObj);
+vidWriteObj2 = VideoWriter(sprintf('sintel_NTS%d_orig.mp4',bsize),'MPEG-4');
+vidWriteObj2.FrameRate = vidReadObj.FrameRate;
+open(vidWriteObj2);
 aframe = readFrame(vidReadObj);
-for i = 1:24
+for i = 1:240
     tframe = aframe;
     aframe = readFrame(vidReadObj);
+    writeVideo(vidWriteObj2, aframe);
     for j = 1:3
     [pframe, mvframe] = NewThreeStep(aframe(:,:,j),tframe(:,:,j),bsize);
     pframes(:,:,j) = pframe;
@@ -19,4 +22,5 @@ for i = 1:24
     writeVideo(vidWriteObj, pframes);
 end
 close(vidWriteObj);
+close(vidWriteObj2);
    
